2eaa53deb3a40cfbb7f4ea3b718e05b178d28435
dyindude <dyindude@github.dyindude.net>
2016-04-07 14:02:43 -0500

    first attempt at array_front9(). test seems to fail. needs debugging.

---
+++ warmup-2/array_front9/array_front9.go
@@ -0,0 +1,20 @@
+package array_front9
+
+func array_front9(nums []int) bool {
+	var max = 4
+	if len(nums) < 4 {
+		max = len(nums)
+	} else {
+		max -= 1
+	}
+
+	for i := 0; i <= max; i++ {
+		if nums[i] == 9 {
+			return true
+		} else {
+			return false
+		}
+
+	}
+	return false
+}
---
+++ warmup-2/array_front9/array_front9_test.go
@@ -0,0 +1,42 @@
+package array_front9
+
+import "testing"
+
+func TestMain(t *testing.T) {
+	t.Log("Testing output of array_front9")
+	s := [][]int{
+		{1, 2, 9, 3, 4},
+		{1, 2, 3, 4, 9},
+		{1, 2, 3, 4, 5},
+		{9, 2, 3},
+		{1, 9, 9},
+		{1, 2, 3},
+		{1, 9},
+		{5, 5},
+		{2},
+		{9},
+		{},
+		{3, 9, 2, 3, 3},
+	}
+	r := []bool{
+		true,
+		false,
+		false,
+		true,
+		true,
+		false,
+		true,
+		false,
+		false,
+		true,
+		false,
+		true,
+	}
+	for i, v := range r {
+		if array_front9(s[i][:]) != v {
+			t.Errorf("Passed values: %s", s[i][:])
+			t.Errorf("Expected output of %s, but got %s instead.", v, array_front9(s[i][:]))
+		}
+	}
+
+}
